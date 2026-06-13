187b796a11cc47b39ff30e43d652150c56aa8edb
mikeydebug <cs24b1031@iiitr.ac.in>
2026-06-14 00:26:14 +0530

    fix: update theme import path in layout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from 'expo-router';
 import { View, StyleSheet } from 'react-native';
-import { theme } from '../constants/theme';
+import { theme } from '../src/constants/theme';
 
 export default function Layout() {
   return (
